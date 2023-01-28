5cb5f9c40576847f076d9a115e2c5fe155a3d6ad
Narue-GS <naruegit@gmail.com>
2023-01-28 18:55:08 -0300

    init aplication development

---
+++ main.py
@@ -0,0 +1,43 @@
+import smtplib
+import email.message
+from flask import Flask
+from flask_apscheduler import APScheduler
+
+app = Flask(__name__)
+cron = APScheduler()
+
+
+@app.route("/")
+def index():
+    return "running"
+
+
+def send_email():  
+    email_text = """
+        <h1>Hey bro</h1></br>
+        <p>What's up?</p>
+    """
+
+    msg = email.message.Message()
+    msg['Subject'] = "Assunto"
+    msg['From'] = 'myemailbot999@gmail.com'
+    msg['To'] = 'narutogbzlol@gmail.com'
+    password = 'buqgsrnqrxjjiggq' 
+    msg.add_header('Content-Type', 'text/html')
+    msg.set_payload(email_text)
+
+    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s.starttls()
+    # Login Credentials for sending the mail
+    s.login(msg['From'], password)
+    s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    print('Email enviado')
+
+def cronFunc():
+    send_email()
+if __name__=="__main__":
+    cron.add_job(id="cron func", func = cronFunc, trigger="interval", seconds=3600)
+    cron.start()
+    app.run(debug=True, use_reloader=False)
+
+
