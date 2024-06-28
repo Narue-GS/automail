6cade79a4195c3a923d7d4d414e6e42c24ed5075
Naruê Gonçalves Santos <naruegit@gmail.com>
2024-06-28 20:35:23 -0300

    feat:multi-target sending applyed. Still unsafe tho

--- main.py
+++ main.py
@@ -1,50 +1,59 @@
+import os
 import smtplib
 import email.message
 from flask import Flask
 
-from transformers import GPT2Tokenizer, GPT2Model
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2Model.from_pretrained('gpt2')
-text = "Say something funny about ducks"
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
-response = tokenizer.decode(output[0], skip_special_tokens=True)
-print(response)
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+key = "AIzaSyDAQDS5fLJ5HpUJT-j59rc0FoNFvgnRtmE"
+genai.configure(api_key=key)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+contacts = ['marigumerpr@gmail.com', 'narutogbzlol@gmail.com']
+
+def to_markdown(text):
+  text = text.replace('•', '')
+  text = text.replace(':', '<br/>')
+  return Markdown(textwrap.indent(text, '\n', predicate=lambda _: True))
 
 # Texto de entrada
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route("/")
-# def index():
-#   return send_email()
+@app.route("/")
+def index():
+  send_email(contacts)
+  return "send"
 
-# def generate():
-#     input_text = "Give me a curiosity about ducks"
-#     input_ids = tokenizer.encode(input_text, return_tensors='pt')
+def generate():
+  response = model.generate_content("Give me 5 curiosities about ducks, in portuguese, formated in HTML5")
+  return response.text
 
-#     output = model.generate(input_ids, max_length=100, num_return_sequences=1, no_repeat_ngram_size=2)
+def send_email(contacts):  
+    email_text = generate()
+    
+    for i in contacts:
+      msg = email.message.Message()
+      msg['Subject'] = "Sua fonte de curiosidades sobre patos :)"
+      msg['From'] = 'myemailbot999@gmail.com'
+      msg.set_payload(to_markdown(email_text).data)
+      msg.add_header('Content-Type', 'text/html')
+      password = 'buqgsrnqrxjjiggq'
+      s = smtplib.SMTP('smtp.gmail.com: 587')
+      s.starttls()
+      s.login(msg['From'], password)
+      msg['To'] = i
+      print(msg['To'])
+      s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
+    return email_text
 
-#     response = tokenizer.decode(output[0], skip_special_tokens=True)
-#     return response
-
-# def send_email():  
-#     email_text = generate()
-#     msg = email.message.Message()
-#     msg['Subject'] = "Assunto"
-#     msg['From'] = 'myemailbot999@gmail.com'
-#     msg['To'] = 'narutogbzlol@gmail.com'
-#     password = 'buqgsrnqrxjjiggq'
-#     msg.add_header('Content-Type', 'text/html')
-#     msg.set_payload(email_text)
-
-#     s = smtplib.SMTP('smtp.gmail.com: 587')
-#     s.starttls()
-#     s.login(msg['From'], password)
-#     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-#     return email_text
-
-# if __name__=="__main__":
-#     app.run(debug=True, use_reloader=False)
+if __name__=="__main__":
+    app.run(debug=True, use_reloader=False)
 
 
